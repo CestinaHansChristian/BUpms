1a9b12599d4d5c041de94c989d3e5bce608a8529
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-08-17 02:17:49 +0800

    💿 feat(login): add new login endpoint and authentication logic
    This commit introduces a new `login.post.ts` endpoint that handles user authentication. The endpoint reads the request body, which includes the user's email and password, and returns an object with the provided credentials after a 1-second delay to simulate a server-side authentication process.
    
    Additionally, this commit updates the `nuxt.config.ts` file to enable Nuxt.js devtools, which can be useful for debugging and development purposes.
    
    The `package.json` file is also updated to include the `pocketbase` dependency, which will likely be used for managing user authentication and data storage in future commits.
    
    Finally, the `SideBarLogin.vue` component is updated to display the current authentication status, and the `index.vue` page is modified to include the `LoginSideBarLogin` and `LoginForm` components.

--- BUpms/layouts/Landing.vue
+++ BUpms/layouts/Landing.vue
@@ -14,15 +14,18 @@
                 <img src="assets/BU_title.png" alt="" class="object-contain min-w-32">
                 <div class="header-2-wrapper flex place-self-center gap-x-5 px-3 min-w-32 ">
                     <div class="name-container flex place-items-center text-sky-500 font-semibold text-sm md:text-2xl ">
-                        Welcome, User
+                        Welcome, {{ $auth.isAuthenticated }}
                     </div>
                     <div @click="show_alert_notify" class="notification-container cursor-pointer relative ">
                         <IconsNotification></IconsNotification>
                         <div class="notification h-3 w-3 bg-orange-500 rounded-full absolute top-0 right-0"></div>
                         <div v-if="alertIsClicked" class="notif_list fixed right-0 z-10">
-                            <div class="list-container bg-slate-300 w-48 md:w-72 me-5 rounded-lg shadow-md shadow-gray-400">
+                            <div
+                                class="list-container bg-slate-300 w-48 md:w-72 me-5 rounded-lg shadow-md shadow-gray-400">
                                 <div class="notif-wrapper p-3 text-sm md:text-xl">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus nesciunt impedit provident illo, ipsum cumque iusto illum harum eaque, accusamus nemo veritatis molestiae optio temporibus sapiente. Id rerum nobis quidem!
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus nesciunt impedit
+                                    provident illo, ipsum cumque iusto illum harum eaque, accusamus nemo veritatis
+                                    molestiae optio temporibus sapiente. Id rerum nobis quidem!
                                 </div>
                             </div>
                         </div>
@@ -35,23 +38,24 @@
     </div>
 </template>
 <style scoped>
-    .router-link-active {
-        background-color: skyblue;
-    }
-
+.router-link-active {
+    background-color: skyblue;
+}
 </style>
 <script>
-    export default {
-        name: 'Landing',
-        data() {
-            return {
-                alertIsClicked: false
-            }
-        },
-        methods: {
-            show_alert_notify() {
-                this.alertIsClicked = !this.alertIsClicked
-            }
+
+export default {
+    name: 'Landing',
+    data() {
+        return {
+            alertIsClicked: false,
         }
-    }
+    },
+    methods: {
+        show_alert_notify() {
+            this.alertIsClicked = !this.alertIsClicked
+        }
+    },
+
+}
 </script>
