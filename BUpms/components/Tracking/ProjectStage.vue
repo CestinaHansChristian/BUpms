32831d20a10348dd678b35745ae401e4575cdfe6
CestinaHansChristian <hanschristiancaratao.cestina@bicol-u.edu.ph>
2024-08-21 22:20:19 +0800

    tracking reactivity partially fixed

--- BUpms/components/Tracking/ProjectStage.vue
+++ BUpms/components/Tracking/ProjectStage.vue
@@ -1,13 +1,38 @@
 <script setup>
+    const {projectStage} = defineProps({
+        projectStage: Object,
+        require: true
+    })
+    const isChecked = reactive({
+        active: true,
+        'bg-sky-400': true,
+    })
+    const isPending = reactive({
+        active: true,
+        'bg-orange-400': true
+    })
+    const projectOneIsActive = computed(()=> {
+        return projectStage.stageOne ? isChecked : isPending
+    })
+
+    const projectTwoIsActive = computed(()=> {
+        return projectStage.stageTwo ? isChecked : isPending
+    })
+
+    const projectThreesActive = computed(()=> {
+        return projectStage.stageThree ? isChecked : isPending
+    })
+
 
 </script>
 <template>
+    {{ projectOneIsActive }}
     <div class="main-container grid place-items-center">
         <div class="gap-x-2 relative flex place-items-center">
             <div class="circle-stage-success-wrapper z-10">
                 <div class="circle bg-sky-600 h-14 w-14 rounded-full  place-content-center grid">
                     <div class="circle-inner h-12 w-12 bg-sky-400 rounded-full grid place-items-center">
-                        check
+                        Start
                     </div>
                 </div>
                 <div class="project-stage-description absolute w-20 text-xs uppercase">
@@ -16,8 +41,8 @@
             </div>
             <div class="circle-stage-one-wrapper z-10">
                 <div class="circle bg-sky-600 h-14 w-14 rounded-full  place-content-center grid">
-                    <div class="circle-inner h-12 w-12 bg-sky-400 rounded-full grid place-items-center">
-                        check
+                    <div :class="projectOneIsActive" class="circle-inner h-12 w-12 rounded-full grid place-items-center">
+                        1
                     </div>
                 </div>
                 <div class="project-stage-description ps-2 absolute w-16 text-xs uppercase">
@@ -26,8 +51,8 @@
             </div>
             <div class="circle-stage-two-wrapper z-10">
                 <div class="circle bg-sky-600 h-14 w-14 rounded-full  place-content-center grid">
-                    <div class="circle-inner h-12 w-12 bg-sky-400 rounded-full grid place-items-center">
-                        check
+                    <div :class="projectTwoIsActive" class="circle-inner h-12 w-12  rounded-full grid place-items-center">
+                        2
                     </div>
                 </div>
                 <div class="project-stage-description ps-2 absolute w-16 text-xs uppercase">
@@ -36,8 +61,8 @@
             </div>
             <div class="circle-stage-three-wrapper z-10">
                 <div class="circle bg-sky-600 h-14 w-14 rounded-full  place-content-center grid">
-                    <div class="circle-inner h-12 w-12 bg-sky-400 rounded-full grid place-items-center">
-                        check
+                    <div :class="projectThreesActive" class="circle-inner h-12 w-12 rounded-full grid place-items-center">
+                        3
                     </div>
                 </div>
                 <div class="project-stage-description ps-2 absolute w-16 text-xs uppercase">
@@ -46,8 +71,8 @@
             </div>
             <div class="circle-stage-approved-wrapper z-10">
                 <div class="circle bg-sky-600 h-14 w-14 rounded-full  place-content-center grid">
-                    <div class="circle-inner h-12 w-12 bg-sky-400 rounded-full grid place-items-center">
-                        check
+                    <div class="circle-inner h-12 w-12 bg-sky-800 rounded-full grid place-items-center">
+                        Final
                     </div>
                 </div>
                 <div class="project-stage-description absolute h-12 w-12 text-xs uppercase">
--- BUpms/pages/Tracking.vue
+++ BUpms/pages/Tracking.vue
@@ -1,14 +1,22 @@
 <script setup>
+
+    // paste data here
     definePageMeta({
         layout: 'landing'
     })
     
     const whatOrg = ref('TUPAD')
+    const documentStatus = ref({
+        stageOne: true,
+        stageTwo: true,
+        stageThree: false,
+    })
+
 </script>
 <template>
     <div class="h-screen grid">
         <div class="project-icons grid">
-            <TrackingProjectStage></TrackingProjectStage>
+            <TrackingProjectStage :project-stage="documentStatus"></TrackingProjectStage>
         </div>
         <div class="project-description grid place-content-center">
             <TrackingProjectMeta :organization="whatOrg"></TrackingProjectMeta>
