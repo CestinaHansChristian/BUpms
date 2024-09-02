4acc16b8f5e49ae1bc81a1f9002433256a5c2aec
CestinaHansChristian <129077009+CestinaHansChristian@users.noreply.github.com>
2024-09-02 23:22:30 +0800

    "Refactored ProjectStage.vue and ExIcon.vue components, updating styles, classes, and conditional logic."

--- BUpms/components/icons/ExIcon.vue
+++ BUpms/components/icons/ExIcon.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="6" stroke="white" class="size-7">
-=======
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="6" stroke="white" class="size-7 md:size-20">
->>>>>>> e82002b3befa7eefa6734021f07acc39ec867e28
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
           </svg>          
     </div>
